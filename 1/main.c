a5a91d5eb190e56b71ba8a73e1f9d4d3700c460a
George Kent Allen <main@georgeallen.net>
2024-05-11 16:51:18 -0700

    day1 part2 c

--- 1/main.c
+++ 1/main.c
@@ -3,9 +3,13 @@
 int main() {
 	int character = ' ';
 	int floor = 0;
+	int character_position = 1;
+	int index_of_first_basement_visit = -1;
+	int has_been_in_basement = 0;
 
 	while(character != EOF) {
 		character = getchar();
+
 		if (character == '(') {
 			floor++;
 		}
@@ -13,9 +17,21 @@ int main() {
 		if (character == ')') {
 			floor--;
 		}
-	}
+		
+		if (has_been_in_basement) { 
+			continue;
+		};
 
+		if (floor < 0) {
+			has_been_in_basement = 1;
+			index_of_first_basement_visit = character_position;
+		}
+		character_position++;
+
+
+	}
 	printf("floor = %d", floor);
+	printf("index = %d", index_of_first_basement_visit);
 }
 
 
